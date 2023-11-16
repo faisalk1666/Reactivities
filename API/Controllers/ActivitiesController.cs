3a07d13a91c2e6fd0ed6083f6219a89664234c88
Faisal Khan <Faisal.Khan@investisdigital.com>
2023-11-16 11:00:13 +0000

    Added React (TS + Vite) and Added Post and Delete Method

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,17 +11,60 @@ namespace API.Controllers
         public ActivitiesController(DataContext context)
         {
             _context = context;
-            
+
         }
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return await _context.Activities.ToListAsync();
         }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await _context.Activities.FindAsync(id);
         }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddActivity(string Title, string Description, string City, string Venue)
+        {
+            try
+            {
+                var activity = new Activity
+                {
+                    Title = Title,
+                    Date = DateTime.UtcNow,
+                    Description = Description,
+                    City = City,
+                    Venue = Venue
+                };
+
+                await _context.Activities.AddAsync(activity);
+                await _context.SaveChangesAsync();
+
+                return Ok("Activity added successfully");
+            }
+            catch (Exception ex)
+            {
+               return StatusCode(500, "Internal Server Error"); // Return an appropriate status code and message for an error
+            }
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(Guid id)
+        {
+            try
+            {
+                var activity = await _context.Activities.FindAsync(id);
+                _context.Activities.Remove(activity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+            return Ok("Activity Deleted Successfully");
+        }
+
     }
 }

f13b8c299f0757e864c506ddb177cf22ba9e9d97
Faisal Khan <khanfk1666@gmail.com>
2024-01-05 10:37:28 +0000

    API consumed on frontend using semantic ui

--- API/DTOs/ActivityDTO.cs
+++ API/DTOs/ActivityDTO.cs
@@ -2,6 +2,7 @@
 
 public class ActivityDTO
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public DateTime Date { get; set; }
     public string Description { get; set; }
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,34 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { Header, Icon, List, ListItem } from 'semantic-ui-react';
 
 function App() {
-  
+
+  const [activities, setActivity] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivity(response.data)
+      })
+  }, [])
+
   return (
-    <h1>Reactivities</h1>
+    <div>
+      <Header as='h2' icon textAlign='center'>
+      <Icon name='users' circular />
+      <Header.Content>Reactivities</Header.Content>
+    </Header> 
+      <div>
+        <List celled>
+          {activities.map((activity:any) => (
+            <ListItem key={activity.id}>{activity.title}</ListItem>
+          ))}
+        </List>
+      </div>
+    </div>
   )
 }
 

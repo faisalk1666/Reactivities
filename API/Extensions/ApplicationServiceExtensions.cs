1adbbfd9ebe029204b9eb0bde836d83884f1f1b2
Faisal Khan <khanfk1666@gmail.com>
2024-01-11 10:40:56 +0000

    Added Service Extension Class

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+        services.AddSwaggerGen();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+        });
+        services.AddMediatR(config =>
+                config.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+
+        return services;
+    }
+}
